c816e7fe37ef6abf06a7abbcc00bb1ff55d2089a
UnicornDad8 <unicorndad8@gmail.com>
2025-04-25 18:35:10 -0300

    Agregar MAANG imagen

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,8 +6,9 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
         <Navbar />
-        <div>
+        <div className="flex flex-col items-center justify-center h-[calc(100vh - 5rem)] pointer-events-none select-none">
           {children}
+          <img src="/hero.png" alt="Hero section" />
         </div>
       </div>
     </div>
